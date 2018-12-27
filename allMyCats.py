bd15994cefbfa9a90fcc648b9be7c631240995a3
mikekenndy <mike.kennedy.dev@gmail.com>
2018-12-27 16:26:43 -0500

    Some practice projects

---
+++ allMyCats.py
@@ -0,0 +1,20 @@
+# practice with arrays
+
+#--------------------
+# variable assignment
+#--------------------
+
+cat = ['fat', 'orange', 'loud']
+size, color, disposition = cat
+
+# catNames = []
+# while True:
+#     print('Enter the name of the cat ' + str(len(catNames) + 1)
+#           + ' Or enter nothing to stop.')
+#     name = input()
+#     if name == '':
+#         break
+#     catNames = catNames + [name] # creating array
+# print('The cat names are:')
+# for name in catNames:
+#     print(' ' + name)
---
+++ comma_code.py
@@ -0,0 +1,26 @@
+# takes a list and returns the values separated by commas
+
+def commas(list):
+    temp = list
+    retString = ''
+    temp.insert(len(list) - 1, 'and')
+    for i in list:
+        retString += i + ', '
+    retString = retString[0:len(retString)-2]
+    return retString
+
+# Get list from user
+print('Input list items, press enter when done')
+myStuff = []
+i = 1;
+
+while(True):
+    print('Item ' + str(i) + ': ', end = '')
+    i = i+1
+    item = input()
+    if(item == ''):
+        break
+    myStuff.append(item)
+
+sortedList = commas(myStuff)
+print(sortedList)
